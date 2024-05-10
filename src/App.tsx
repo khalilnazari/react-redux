b65b1277bd290ce836459dff18487e24c836ab0c
khalilnazari <khalil.deve@gmail.com>
2024-05-11 02:22:55 +0800

    Let's do it with Typescript :)

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return <Typography variant="h1">Hello App</Typography>;
+}
+
+export default App;
--- src/store/rootReducers.js
+++
@@ -1,7 +0,0 @@
-import counterSlice from "./reducers/counterSlice";
-import postSlice from "./reducers/postSlice";
-
-export const reducers = {
-  counter: counterSlice,
-  posts: postSlice,
-};
